f50563ee60920445ffed11c86e894f6054cd4fdb
FahriArsiansyah <fahriarsiansyah@gmail.com>
2024-02-23 18:12:51 +0700

    done pak

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,8 @@
+<?php 
+$npm = 54;
+echo "Aku adalah angka <b>54<b>";
+echo "Jika aku dikali 5, maka aku sekarang menjadi <b>" . ($npm *=5) . "</b> <br>";
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>" . ($npm /=2) . "</b> <br>";
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <b>" . ($npm +=75) . "</b> <br>";
+echo "Jika aku ditambah 20, maka aku sekarang menjadi <b>" . ($npm -=20) . "</b> <br>";
+?>
