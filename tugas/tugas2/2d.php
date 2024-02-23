b1e42ecf49185c22ca040506b02a8725f7d0c72f
FahriArsiansyah <fahriarsiansyah@gmail.com>
2024-02-23 19:59:29 +0700

    done ygy

---
+++ tugas/tugas2/2d.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>catur</title>
+    <style>
+        .warna1 {
+            height: 40px;
+            width: 4opx;
+            background-color: white;
+        }
+
+        .warna2 {
+            height: 40px;
+            width: 40px;
+            background-color: black;
+        }
+    </style>
+</head>
+
+<body>
+    <table border="1" cellspacing="0" cellpadding="40">
+        <?php
+        for ($i = 1; $i <= 5; $i++) :
+        ?>
+
+            <tr>
+                <?php for ($j = 1; $j <= 5; $j++) : ?>
+                    <?php $ij = $i + $j; ?>
+                    <?php if ($ij % 2 == 0) : ?>
+                        <td class="warna1"></td>
+                    <?php else : ?>
+                        <td class="warna2"></td>
+                    <?php endif ?>
+
+
+                <?php endfor ?>
+            </tr>
+        <?php endfor ?>
+
+    </table>
+</body>
+
+</html>
